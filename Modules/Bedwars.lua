89374c9f5a301c506decd559da37c4c85d321715
Ln3242 <109390328+Ln3242@users.noreply.github.com>
2022-12-22 16:25:29 -0600

    Update Bedwars.lua

--- Modules/Bedwars.lua
+++ Modules/Bedwars.lua
@@ -328,6 +328,95 @@ MovementSection:NewToggle("fly", "HypixelFly", function(v)
         end
     end)
 
+MovementSection:NewToggle("AutoToxic", "lol", function(state)--springs
+    if state then
+			repeat task.wait() until game:IsLoaded()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LN3242/ChatBxpass/main/AntiBan.lua"))()
+local words = {
+    ["hitler"] = "Нiт1ея",
+    ["nigga"] = "И1ggа",
+    ["nigger"] = "И1 gg ег",
+    ["niggers"] = "И1 gg ег s",
+    ["bitch"] = "В1тсh",
+    ["cum"] = "Сuм",
+    ["rape"] = "Rаре",
+    ["blowjob"] = "Вl○шjob",
+    ["vape"] = "@vape",
+    ["cunt"] = "сцлt",
+    ["fuck"] = "Fчск",
+    ["pussy"] = "рчs2у",
+    ["suicide"] = "sцiс1dе",
+    ["mf"] = "мf",
+    ["loli"] = "l○li",
+    ["ph"] = "рh",
+    ["kill"] = "k!ll",
+    ["date"] = "dате",
+    ["raped"] = "Rаре d",
+    ["slut"] = "sl ц т",
+    ["cumming"] = "Сuм мi иg",
+    ["asshole"] = "а__н○le",
+    ["dick"] = "d¡ с к",
+    ["piss"] = "р1_ss",
+    ["penis"] = "рeи_!s",
+    ["whore"] = "w н ○ я e",
+    ["discord"] = "|)is(|ord",
+    ["hentai"] = "не n тai",
+    ["xvideo"] = "хvi dе○",
+    ["fucking"] = "Fчск y пg",
+    ["nazi"] = "Ñaz¡",
+    ["black"] = "YOUR SO BLACK L"
+}
+local remote = game:GetService("ReplicatedStorage"):FindFirstChild("SayMessageRequest",true)
+local remotebypass
+remotebypass = hookmetamethod(remote,"__namecall",function(self,...)
+    local method = getnamecallmethod()
+    local args = {...}
+    if self == remote and method == "FireServer" then
+        local msg = args[1]
+        local split = string.split(msg," ")
+        local final = ""
+        for _,x in pairs(split) do
+            for _, bypass in pairs(words) do
+                if x:lower() == _ then
+                    if x:upper() ~= x then
+                        msg = msg:gsub(x,bypass)
+                        final = msg
+                    else
+                        msg = msg:gsub(x,bypass):upper()
+                        final = msg:gsub(x,bypass):upper()
+                    end
+                end
+            end
+        end
+        if final ~= "" then
+            args[1] = final
+        end
+        return remotebypass(self,unpack(args))
+    end
+    return remotebypass(self,...)
+end)
+			
+			_G.Speed1 = true
+    wait(1)
+    for i, player in ipairs(game.Players:GetChildren()) do
+local function chat(msg)
+    game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(msg, "All")
+end
+
+chat("Imagine not using script LiquidBounce")
+wait(2.5)
+chat("Imagine dying to script LiquidBounce L")
+wait(2.5)
+chat("I like minors boobs")
+wait(2.5)
+chat("niggers")
+wait(2.5)
+ end
+    else
+		_G.Speed1 = false
+    end
+end)
+
 
 MovementSection:NewKeybind("Highjump", "Highjump", Enum.KeyCode.H, function()--springs
 	local Velocity = Instance.new("BodyVelocity",game.Players.LocalPlayer.Character.HumanoidRootPart)
